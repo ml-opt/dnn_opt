8bed523860240fadd25229033cc83a533d76bbc5
Jairo Rojas Delgado <jrojasdelgado@bcamath.org>
2021-01-22 12:33:42 +0100

    Fixup core rastrigin

--- src/core/solutions/bench/rastrigin.cpp
+++ src/core/solutions/bench/rastrigin.cpp
@@ -24,13 +24,13 @@ float rastrigin::calculate_fitness()
 {
   int n = size();
   float* params = get_params();
-  float result = 10 * size();
+  float result = 10 * n;
 
   solution::calculate_fitness();
 
   for(int i = 0; i < n; i++)
   {
-    result += pow(params[i], 2) - 10 * cos(2 * 3.141592653f * params[i]);
+    result += pow(params[i], 2.0f) - 10.0f * cos(2.0f * 3.141592653f * params[i]);
   }
 
   return result;
